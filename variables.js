49063da079a7f75b08579f8b6b421de4364e3338
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-28 10:27:58 -0600

    commit

--- variables.js
+++ variables.js
@@ -8,10 +8,16 @@
 // console.log(name);
 // console.log(typeof(name));
 
-console.log(10 + "20");
-console.log(10 -"20");
-console.log(true + true);
-console.log(true + false);
-console.log("java " +"Script");
+// console.log(10 + "20");
+// console.log(10 -"20");
+// console.log(true + true);
+// console.log(true + false);
+// console.log("java " +"Script");
 
+var myName = null;
+console.log(myName);
+console.log(typeof(null));
 
+var iAmUseless = null
+console.log(typeof(iAmUseless));
+console.log(typeof(iAmUseless));
