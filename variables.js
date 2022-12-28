74205ee2ae762c11f5fd23a19f63db554de3a9c0
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-27 22:51:33 -0600

    commit

--- variables.js
+++ variables.js
@@ -4,6 +4,14 @@
 
 // console.log("answer: "  + z)
 
-var name = "Nasim Uddin Qureshi";
-console.log(name);
-console.log(typeof(name));
+// var name = "Nasim Uddin Qureshi";
+// console.log(name);
+// console.log(typeof(name));
+
+console.log(10 + "20");
+console.log(10 -"20");
+console.log(true + true);
+console.log(true + false);
+console.log("java " +"Script");
+
+
